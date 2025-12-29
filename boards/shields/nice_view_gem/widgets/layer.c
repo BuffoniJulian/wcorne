29af46a1474c56b67db317c6534e79828079a3cd
Julian Buffoni <julian_buffoni@protonmail.com>
2025-12-29 20:22:49 -0300

    tfdyg

--- boards/shields/nice_view_gem/widgets/layer.c
+++ boards/shields/nice_view_gem/widgets/layer.c
@@ -6,16 +6,15 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_dsc;
     init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
 
-    char text[10] = {};
+    char text[12] = {};
 
     if (state->layer_label == NULL) {
-        sprintf(text, "Layer %i", state->layer_index);
+        sprintf(text, "LAYER %i", state->layer_index);
     } else {
-        strncpy(text, state->layer_label, 9);
+        strncpy(text, state->layer_label, 11);
         to_uppercase(text);
     }
 
-    // Draw at position 17 from top of bottom canvas (146 + BUFFER_OFFSET_BOTTOM = 17)
-    // This places it near the top of the 68px bottom section
-    lv_canvas_draw_text(canvas, 0, 17, 68, &label_dsc, text);
+    // Draw near top of bottom canvas (same as nice_view)
+    lv_canvas_draw_text(canvas, 0, 5, 68, &label_dsc, text);
 }
